468c641160721a407706ce03939b2ba0356c08a5
psci <b.m.tanygin@gmail.com>
2016-07-17 14:10:41 +0300

    Copyright notice

--- fflsym/src/fflsym.cpp
+++ fflsym/src/fflsym.cpp
@@ -1,10 +1,21 @@
-//============================================================================
-// Name        : fflsym.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/*
+  Copyright (C) 2012 Alexander (Polyakov) Peletskyi
+
+  This file is part of FFLCU.
+
+  FFLCU is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  FFLCU is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <iostream>
 #include <fflexprm.h>
